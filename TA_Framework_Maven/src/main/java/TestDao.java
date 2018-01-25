c88deb2c3d35b71a3154c485ed3c98e269448595
Antonio Dotson <antoniomdotson@gmail.com>
2018-01-25 14:20:14 -0500

    Tony's Database

---
+++ TA_Framework_Maven/src/main/java/TestDao.java
@@ -0,0 +1,51 @@
+import java.util.Iterator;
+
+import com.revature.model.Batch;
+import com.revature.util.BatchDaoImp;
+import com.revature.util.TrainerDaoImp;
+
+public class TestDao {
+
+	public static void main(String[] args) {
+		
+		TrainerDaoImp tra = new TrainerDaoImp();
+
+	     
+	      /* List down all the employees */
+		for (int i = 0; i < tra.getAllTrainer().size(); i++) {
+	    	  System.out.println(tra.getAllTrainer().get(i));
+	    	}
+	            
+	      
+	      /* Update employee's records 
+	      tra.updateTrainer("Olivia", "John", "Experience in programming", "java,sql,j2ee,php",2);
+	      
+	        Delete an employee from the database - 
+	        tra.deleteTrainer(1); */
+
+	      
+	   BatchDaoImp batch = new BatchDaoImp();
+	      
+	      Iterator<Batch> it = batch.getFullBatch().iterator();
+	      while (it.hasNext()) {
+	        System.out.println((it.next()));
+	      }
+	      
+	/*      for (int i = 0; i < batch.getFullBatch().size(); i++) {
+	   	  System.out.println(batch.getFullBatch().get(i));
+	     	}
+	            
+	       }
+
+	        
+	        // TODO: First create your model and add to models ArrayList, to prevent NullPointerException for trying this example
+
+	        // Print the name from the list....
+	     */   
+	   }
+	}
+
+		
+
+
+
---
+++ TA_Framework_Maven/src/main/java/com/revature/util/BatchDao.java
@@ -0,0 +1,22 @@
+package com.revature.util;
+
+
+
+import java.util.List;
+
+import com.revature.model.Batch;
+
+
+
+public interface BatchDao {
+
+
+	public void updateBatch(String batchName, String batchCurr,String batchTrainer, String batchLoc, String batchBuild,int batchRoom,
+			String batchStart, String batchEnd);
+
+	public void deleteBatch(String batchName);
+
+	public List<Batch> getFullBatch();
+	
+
+}
