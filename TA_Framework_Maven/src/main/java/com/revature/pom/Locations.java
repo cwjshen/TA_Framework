e72edb943eb84dbbcac0daab4e14b854bde3fb88
aaleung <aaleung@umich.edu>
2018-01-26 02:03:22 -0500

    Test

--- TA_Framework_Maven/src/main/java/com/revature/pom/Locations.java
+++ TA_Framework_Maven/src/main/java/com/revature/pom/Locations.java
@@ -12,6 +12,7 @@ public class Locations {
 	public static WebElement RevatureHQ(WebDriver d) {
 		return d.findElement(By.xpath("span[@aria-label='Revature HQ Reston, VA Reston VA']"));
 	}
+	
 		public static WebElement Reston11730(WebDriver d) {
 			return d.findElement(By.xpath("span[@aria-label='Reston 11730']"));
 		}
@@ -41,6 +42,7 @@ public class Locations {
 	public static WebElement NewYorkCity(WebDriver d) {
 		return d.findElement(By.xpath("span[@aria-label='New York City New York, NY New York NY']"));
 	}
+	
 		public static WebElement CunyQueens(WebDriver d) {
 			return d.findElement(By.xpath("span[@aria-label='CUNY QUEENS']"));
 		}
