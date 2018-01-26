181b6e556e668a365299bbc5096e9131d9254a5b
aaleung <aaleung@umich.edu>
2018-01-26 14:36:44 -0500

    TestLocation created and updated TestBatch

---
+++ TA_Framework_Maven/src/main/java/com/revature/TestLocations.java
@@ -0,0 +1,59 @@
+package com.revature;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.revature.cuketests.OverviewCukes;
+import com.revature.cuketests.LocationsCukes;
+import com.revature.pom.Login;
+import com.revature.pom.Logout;
+import com.revature.util.DriverUtil;
+import com.revature.util.LoginUtil;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+//Hook Class to run Cucumber Tests
+@CucumberOptions(features="src/test/java")
+public class TestLocations extends TrainerSuite {
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Running Overview Tab Test");
+
+	}
+	
+	@Test
+	public void TestOne() {
+		try {
+			// Need assert statements for these? Idk though because they can never be false
+			//	since it would just jump to catch block with NoSuchElementException
+			LocationsCukes.clickLocations(wd);
+			LocationsCukes.RevatureHQExists(wd);
+			LocationsCukes.NewYorkCityExists(wd);
+
+		} catch (Throwable e) { 
+			System.out.println("Revature HQ and/or New York City drop downs not found. Not in Locations tab");
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("Tests successful. Locations tab working as intended");
+	}
+
+}
+
