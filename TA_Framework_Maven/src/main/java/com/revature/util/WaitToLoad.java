5ad93541426d8a21c2cfb14e52c200d573c0ed65
cwjshen <shenj529@gmail.com>
2018-01-30 16:36:28 -0500

    finished test for invalidating empty form

--- TA_Framework_Maven/src/main/java/com/revature/util/WaitToLoad.java
+++ TA_Framework_Maven/src/main/java/com/revature/util/WaitToLoad.java
@@ -1,7 +1,5 @@
 package com.revature.util;
 
-import java.util.NoSuchElementException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
